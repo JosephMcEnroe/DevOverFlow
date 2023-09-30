96a8d357b7beea8a25942d82ed88fc37f8cf715c
unknown <workjcmcenroe@gmail.com>
2023-09-30 15:46:21 -0400

    auth plus metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,31 @@
 import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+import { Inter, Space_Grotesk } from 'next/font/google'
+import './globals.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
-  title: 'Next.js 13 with Clerk',
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+})
+
+export const metadata: Metadata = {
+  title: 'DevOverFlow',
+  description:
+    'A community-driven platflorm for asking and answering' +
+    ' programming questions. Get help, share knowledge, and collaborate ' +
+    'developers from around the world. Explore topics in web development, ' +
+    'mobile app developement, algorithms, data structures, and more.',
+  icons: {
+    icon: '/public/assets/images/site-logo.svg',
+  },
 }
 
 export default function RootLayout({
@@ -10,9 +34,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient' + 'hover:text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <h1 className="h1-bold">This is a piece of text</h1>
+        <body
+          className={`${inter.variable} 
+        ${spaceGrotesk.variable}`}
+        >
+          <h1 className="h1-bold">This is a piece of text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
